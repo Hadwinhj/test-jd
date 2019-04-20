a4a4e3fb7210fca271a8da387004fa47986ea9c0
Jam He <jamisonline.he@gmail.com>
2019-04-20 21:51:15 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/test-pic.jpg" style="width: 100%;display: block;">
+        <!--<HelloWorld msg="通话记录"/>-->
+        <contact-list title="通话记录"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import ContactList from './components/contact_list.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            // HelloWorld,
+            ContactList
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/contact_list.vue
@@ -0,0 +1,94 @@
+<template>
+
+    <div>
+
+        <!--标题-->
+        <div>{{title}}</div>
+
+        <!--通话记录列表-->
+        <ul class="contact_list_area" style="padding: 0;">
+            <!--通话记录 item -->
+            <li class="contact_list_item" v-for="item in contact">
+                <!--联系人名字-->
+                <div class="contact_list_name">{{item.name}}</div>
+                <!--联系人电话-->
+                <div class="contact_list_phone" v-on:click="clickListenerPhone(item)">{{item.phone}}</div>
+            </li>
+
+        </ul>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "contact_list",
+        props: {
+            title: String
+        },
+        data() {
+            return {
+                contact: [
+                    {
+                        "name": "豇豆",
+                        "phone": 13677889900
+                    },
+                    {
+                        "name": "充个",
+                        "phone": 13677889900
+                    },
+                    {
+                        "name": "都分手",
+                        "phone": 13677889900
+                    },
+                    {
+                        "name": "ss",
+                        "phone": 13677889900
+                    }
+                ]
+            }
+        },
+
+        created: function () {
+
+        },
+
+        methods: {
+
+
+            /**
+             * 拨打电话号码
+             * @param item
+             */
+            clickListenerPhone(item) {
+                console.log("给" + item.name + " 拨打电话号码：" + item.phone)
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+    .contact_list_area {
+
+    }
+
+    .contact_list_item {
+        display: flex;
+        height: 100px;
+        background: burlywood;
+        border-bottom: solid aqua 10px;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .contact_list_name {
+        padding: 30px;
+    }
+
+    .contact_list_phone {
+        font-size: 30px;
+        padding: 30px;
+    }
+</style>
